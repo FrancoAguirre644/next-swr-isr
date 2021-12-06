1f83259d8fe2d680a93da64c4446edb28c94886a
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-12-06 16:00:08 -0300

    Header component added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Header from '../components/Header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
